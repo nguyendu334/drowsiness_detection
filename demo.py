6e8f6499611930d8101b27f53cc64aa70b4d8a59
NguyenDu <nguyendu334@gmail.com>
2022-11-29 22:17:56 +0700

    edit video

--- demo.py
+++ demo.py
@@ -9,7 +9,6 @@ import dlib
 import cv2
 import winsound
 
-
 start_time = time.time()
 
 def ty_so_mat(eye):
@@ -35,21 +34,19 @@ detector = dlib.get_frontal_face_detector()
 PREDICTOR_PATH = "68_face_landmarks_predictor.dat"
 predictor = dlib.shape_predictor(PREDICTOR_PATH)
 
-# lay cac chi so cua cac dau moc tren khuon mat
-# cho mat trai va mat phai tuong ung
+# lay cac chi so cua cac dau moc tren khuon mat cho mat trai va mat phai tuong ung
 (lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
 (rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
 
 # bat dau thu luong video
-#print("[INFO] starting video stream thread...")
+print("[INFO] starting video stream thread...")
 vs = VideoStream(0).start()
 time.sleep(1.0)
 print ( "--- %s seconds ---" % (time.time() - start_time))
 # vong lap qua cac khung hinh tu luong video
 while True:
     start_time_1 = time.time()
-    # lay khung tu luong tep video, thay doi kich thuoc
-    # va chuyen doi no sang cac kenh mau xam
+    # lay khung tu luong tep video, thay doi kich thuoc va chuyen doi no sang cac kenh mau xam
     frame = vs.read()
     frame = imutils.resize(frame, width=800)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -61,22 +58,19 @@ while True:
         # xac dinh diem moc tren mat doi voi vung khuon mat, sau do chuyen doi diem moc tren mat toa do (x, y) voi mangNumPy
         shape = predictor(gray, rect)
         shape = face_utils.shape_to_np(shape)
-        # trich xuat toa do mat trai va phai,
-        # sau do su dung toa do de tinh ti le mat cho ca2 mat
+        # trich xuat toa do mat trai va phai, sau do su dung toa do de tinh ti le mat cho ca2 mat
         leftEye = shape[lStart:lEnd]
         rightEye = shape[rStart:rEnd]
         leftEAR = ty_so_mat(leftEye)
         rightEAR = ty_so_mat(rightEye)
         # tinh ti le mat trung binh cho ca 2 mat
         ear = (leftEAR + rightEAR) / 2.0
-    # tinh vien bao loi cho mat trai va mat phai,
-    # sau do hinh dung ra (ve ra) vien bao do cho moi mat
+    # tinh vien bao loi cho mat trai va mat phai, sau do hinh dung ra (ve ra) vien bao do cho moi mat
         leftEyeHull = cv2.convexHull(leftEye)
         rightEyeHull = cv2.convexHull(rightEye)
         cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
         cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
-    # kiem tra ti le mat co nam duoi nguong nhay mat hay khong,
-    # neu co tang bo dem khung nhap nhay
+    # kiem tra ti le mat co nam duoi nguong nhay mat hay khong, neu co tang bo dem khung nhap nhay
         if ear < NGUONG_MAT: 
             DEM += 1
         # neu nham mat du so luong khung da dat thi bao dong
@@ -87,8 +81,7 @@ while True:
         # mat khac neu ty le mat khong duoi nguong nhay mat
         else: 
             DEM = 0
-        # ve thong so ty le mat da tinh tren khung de giup viec kiem tra
-        # sua loi va thiet lap lai dung nguong ty le mat va bo dem khung
+        # ve thong so ty le mat da tinh tren khung de giup viec kiem tra sua loi va thiet lap lai dung nguong ty le mat va bo dem khung
         cv2.putText(frame, "TY SO MAT: {:.2f}".format(ear), (250, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
         
     # show khung hinh
